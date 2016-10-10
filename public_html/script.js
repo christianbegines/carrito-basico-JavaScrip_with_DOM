e9e7b937040ecdaca2ef6a342f6c6657d40a72c0
christian <christiabeginescarrasco@gmail.com>
2016-10-10 13:55:55 +0200

    Coger precios y mostrarlo

--- public_html/script.js
+++ public_html/script.js
@@ -1,8 +1,17 @@
 function cargarEnCarro(elemento){
-   var div=document.getElementById(elemento);
-   var parrafo=div.getElementsByTagName("p")[0];
-   var nodoTexto=parrafo.firstChild();
-   var texto=nodoTexto.nodeValue();
-   alert(texto);
+    
+    alert(elemento);
+    
+    var div=document.getElementById(elemento);
+    alert(div.getElementsByTagName("p")[0].firstChild.nodeValue);
+  
+       
+   
+   var texto=div.getElementsByTagName("p")[0].firstChild.nodeValue;
+   var parrafoNuevo= document.createElement("p");
+   
+   var nodoTextoNuevo=document.createTextNode(texto);
+   parrafoNuevo.appendChild(nodoTextoNuevo);
+   document.getElementById("precios").appendChild(parrafoNuevo);
 
 }
