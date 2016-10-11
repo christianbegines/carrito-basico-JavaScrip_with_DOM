ac6d5613e57f0f3fdf373b0e6af990aaa6522802
christian <christiabeginescarrasco@gmail.com>
2016-10-11 12:29:39 +0200

    Funciona el carrio, añadir botones

--- public_html/script.js
+++ public_html/script.js
@@ -1,17 +1,49 @@
-function cargarEnCarro(elemento){
-    
-    alert(elemento);
-    
-    var div=document.getElementById(elemento);
-    alert(div.getElementsByTagName("p")[0].firstChild.nodeValue);
-  
-       
-   
-   var texto=div.getElementsByTagName("p")[0].firstChild.nodeValue;
-   var parrafoNuevo= document.createElement("p");
-   
-   var nodoTextoNuevo=document.createTextNode(texto);
-   parrafoNuevo.appendChild(nodoTextoNuevo);
-   document.getElementById("precios").appendChild(parrafoNuevo);
 
+function cargarEnCarro(elemento) {
+    var div = document.getElementById(elemento);
+    var texto = div.getElementsByTagName("p")[0].firstChild.nodeValue;
+    var tipoProducto = "";
+    var precio = texto.toString().split(" ");
+    var srcImagen = div.getElementsByTagName("img")[0].getAttribute("src");
+    var img = document.createElement("img");
+    var botonRestar=document.createElement("button");
+    var botonBorrar=document.createElement("button");
+    img.setAttribute("src", srcImagen);
+    var parrafoNuevo = document.createElement("p");
+    var nodoTextoPrecio = document.createTextNode(texto);
+    var nodoTextoRestar = document.createTextNode("Restar");
+    var nodoTextoBorrar = document.createTextNode("Borrar");
+    var nodoTextoUnidad = document.createTextNode("Unidades:1");
+    botonRestar.appendChild(nodoTextoRestar);
+    botonRestar.setAttribute("onclick","restar()");
+    botonBorrar.appendChild(nodoTextoBorrar);
+    botonBorrar.setAttribute("onclick","borrar()");
+    //parrafoNuevo.appendChild(nodoTextoNuevo);
+    var br = document.createElement("br");
+    var br2 = document.createElement("br");
+    var br3 = document.createElement("br");
+    var inCart = document.getElementById("c" + div.id);
+    if (inCart !== null) {
+        var unidades = parseInt(inCart.split(":")[1]);
+        unidades++;
+        var nodoUnidades = document.createTextNode("Unidades:"+unidades);
+        inCart.removeChild(inCart.lastChild);
+        inCart.appendChild(nodoUnidades);        
+    } else {
+        var divArticulo = document.createElement("div");
+        divArticulo.setAttribute("class", "articulo_carro");
+        divArticulo.setAttribute("id", "c" + div.id);
+        divArticulo.appendChild(img);
+        divArticulo.appendChild(br);
+        divArticulo.appendChild(nodoTextoPrecio);
+        divArticulo.appendChild(br2);
+        divArticulo.appendChild(nodoTextoUnidad);
+        divArticulo.appendChild(br3);
+        divArticulo.appendChild(botonRestar);
+        divArticulo.appendChild(botonBorrar);
+        
+        
+        document.getElementById("precios").appendChild(divArticulo);
+    }
+    
 }
