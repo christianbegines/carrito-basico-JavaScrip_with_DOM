266607d8243d0492d9f5f8d8298c012cff4f411c
christian <christiabeginescarrasco@gmail.com>
2016-10-14 12:42:49 +0200

    Mejoras carrito

--- public_html/script.js
+++ public_html/script.js
@@ -3,38 +3,52 @@ function cargarEnCarro(elemento) {
     var div = document.getElementById(elemento);
     var texto = div.getElementsByTagName("p")[0].firstChild.nodeValue;
     var tipoProducto = "";
-    var precio = texto.toString().split(" ");
+    var precio = parseInt(texto.toString().split(" ")[1]);
+    
     var srcImagen = div.getElementsByTagName("img")[0].getAttribute("src");
     var img = document.createElement("img");
     var botonRestar = document.createElement("button");
     var botonBorrar = document.createElement("button");
     img.setAttribute("src", srcImagen);
-    var parrafoNuevo = document.createElement("p");
+    
     var nodoTextoPrecio = document.createTextNode(texto);
     var nodoTextoRestar = document.createTextNode("Restar");
     var nodoTextoBorrar = document.createTextNode("Borrar");
     var nodoTextoUnidad = document.createTextNode("Unidades:1");
+   
     botonRestar.appendChild(nodoTextoRestar);
     botonRestar.setAttribute("onclick", "restar()");
     botonBorrar.appendChild(nodoTextoBorrar);
-    botonBorrar.setAttribute("onclick", "borrar()");
+    botonBorrar.setAttribute("onclick", "borrarArticulo()");
   
     var br = document.createElement("br");
     var br2 = document.createElement("br");
     var br3 = document.createElement("br");
-    
+    var br4 = document.createElement("br");
+    var precioTotal=0;
+    var nodoPrecioTotal=document.createTextNode("PrecioTotal:"+precio);
     var inCart = document.getElementById("c" + div.id);
 
     if (inCart !== null) {
         //
+       
         var segundoHijoF = inCart.lastChild.previousSibling;
         var tercerHijoF = segundoHijoF.previousSibling;
         var textoUnidadRecogido = tercerHijoF.previousSibling;
         
         var unidades = parseInt(textoUnidadRecogido.nodeValue.split(":")[1]);
+        
         unidades++;
+        precioTotal=precio*unidades;
+        
         var nodoUnidades = document.createTextNode("Unidades:" + unidades);
+        var nodoPrecioTotalC = document.createTextNode("PrecioTotal:" + precioTotal);
         inCart.replaceChild(nodoUnidades,textoUnidadRecogido);
+        var nodoBoton = inCart.lastChild.previousSibiling;
+        console.log(nodoBoton);
+        var nodoPrecioT=nodoBoton.previousSibling;
+        inCart.insertBefore(nodoPrecioTotalC,nodoPrecioT);
+        
     } else {
         var divArticulo = document.createElement("div");
         divArticulo.setAttribute("class", "articulo_carro");
@@ -45,11 +59,17 @@ function cargarEnCarro(elemento) {
         divArticulo.appendChild(br2);
         divArticulo.appendChild(nodoTextoUnidad);
         divArticulo.appendChild(br3);
+        divArticulo.appendChild(nodoPrecioTotal);
+        divArticulo.appendChild(br4);
         divArticulo.appendChild(botonRestar);
         divArticulo.appendChild(botonBorrar);
 
 
         document.getElementById("precios").appendChild(divArticulo);
+    }
+    
+    function borrarArticulo(){
+        
     }
 
 }
